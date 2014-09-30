9f7dd822b1e75fcd3bf723d723c32301f3913873
Daniel Bader <dbader@mobify.com>
2014-09-30 15:19:37 -0700

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
     sys.exit()
 
-PACKAGE_VERSION = '0.0.3'
+PACKAGE_VERSION = '0.1.0'
 PACKAGE_DOWNLOAD_URL = (
     'https://github.com/dbader/jack/tarball/' + PACKAGE_VERSION
 )
